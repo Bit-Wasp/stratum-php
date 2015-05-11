62634ed756398229e12d895aa0ce55a645cd7e48
Thomas Kerin <thomas@bitwasp.co>
2015-05-11 04:56:29 +0100

    Update README and examples

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# stratum-php
+## stratum-php
 Implementation of the Stratum protocol (for electrum and mining) using ReactPHP
+
+Currently this library supports a TCP transport to stratum servers. 
+Examples of these and other servers can be found on the Electrum server list.
+
+Stratum enables rather stateless wallets to be built with minimal effort, and depending on the use-case could replace running a full node.
+ 
+### Example
+```php
+
+use \BitWasp\Bitcoin\Stratum\StratumClient;
+use \BitWasp\Bitcoin\Stratum\Request\RequestFactory;
+
+$requestFactory = new RequestFactory;
+
+$host = 'electrum3.hachre.de';
+$port = 50001;
+
+// Initialize react event loop, resolver, and connector
+$loop = React\EventLoop\Factory::create();
+$dnsResolverFactory = new React\Dns\Resolver\Factory();
+$dns = $dnsResolverFactory->create('8.8.8.8', $loop);
+$connector = new \React\SocketClient\Connector($loop, $dns);
+
+// Initialize TCP transport for stratum client
+$tcp = new \BitWasp\Bitcoin\Stratum\Connector\Tcp($connector, $host, $port);
+$stratum = new StratumClient($tcp, $requestFactory);
+
+// Make the query, receive a Promise
+$t = $stratum->getTransaction('2439243c47803a613728beab5ccfd7a426c9bfdd069d463b28f6f49915801988');
+$t->then(function ($response) {
+    echo $response;
+});
+
+$loop->run();
+
+```
--- examples.php
+++ examples.php
@@ -3,79 +3,30 @@
 require_once "vendor/autoload.php";
 
 use \BitWasp\Bitcoin\Stratum\StratumClient;
+use \BitWasp\Bitcoin\Stratum\Request\RequestFactory;
 
-$requestFactory = new \BitWasp\Bitcoin\Stratum\Request\RequestFactory();
+$requestFactory = new RequestFactory;
 
-$h = 'electrum3.hachre.de';
+$host = 'electrum3.hachre.de';
 $port = 50001;
 
+// Initialize react event loop, resolver, and connector
 $loop = React\EventLoop\Factory::create();
-
 $dnsResolverFactory = new React\Dns\Resolver\Factory();
 $dns = $dnsResolverFactory->create('8.8.8.8', $loop);
-
 $connector = new \React\SocketClient\Connector($loop, $dns);
-$tcp = new \BitWasp\Bitcoin\Stratum\Connector\Tcp($connector, $h, $port);
 
+// Initialize TCP transport for stratum client
+$tcp = new \BitWasp\Bitcoin\Stratum\Connector\Tcp($connector, $host, $port);
 $stratum = new StratumClient($tcp, $requestFactory);
-/*
+
+// Make the query, receive a Promise
 $t = $stratum->getTransaction('2439243c47803a613728beab5ccfd7a426c9bfdd069d463b28f6f49915801988');
 $t->then(function ($response) {
-        echo $response;
-    },
-    function () { echo 'boo'; },
-    function () { echo 'bah'; }
-);
-
-$b = $stratum->getBanner();
-$b->then(function ($response) {
     echo $response;
 },
-    function () { echo 'boo'; },
-    function () { echo 'bah'; }
+function () { /* error callback */ }
 );
 
-$c = $stratum->sendVersion('1.9.7', '0.6');
-$c->then(function ($response) {
-    echo $response;
-},
-    function () { echo 'boo'; },
-    function () { echo 'bah'; }
-);
-
-$d = $stratum->getAddressHistory('1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L');
-$d->then(function ($response) {
-    echo $response;
-},
-    function () { echo 'boo'; },
-    function () { echo 'bah'; }
-);
-
-$e = $stratum->getTransactionMerkle('2439243c47803a613728beab5ccfd7a426c9bfdd069d463b28f6f49915801988', 347315);
-$e->then(function ($response) {
-    echo $response;
-},
-    function () { echo 'boo'; },
-    function () { echo 'bah'; }
-);
-*/
-
-
-/*
-$f = $stratum->getNodeVersion();
-$f->then(function ($response) {
-    echo $response;
-},
-    function () { echo 'boo'; },
-    function () { echo 'bah'; }
-);
-
-$f = $stratum->getServices();
-$f->then(function ($response) {
-    echo $response;
-},
-    function () { echo 'boo'; },
-    function () { echo 'bah'; }
-);*/
 
 $loop->run();
