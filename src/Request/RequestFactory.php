d894aa51d2712f2be77e02a2d28dae8233a5e03d
Thomas Kerin <me@thomaskerin.io>
2016-05-18 02:18:40 +0100

    Return API error during parse, since it's being sent by the remote server

--- src/Request/RequestFactory.php
+++ src/Request/RequestFactory.php
@@ -38,7 +38,7 @@ class RequestFactory
             $id = isset($decoded['id']) ? $decoded['id'] : null;
 
             if (isset($decoded['error'])) {
-                throw new ApiError($id, $decoded['error']);
+                return new ApiError($id, $decoded['error']);
             } elseif (isset($decoded['method']) && isset($decoded['params'])) {
                 return new Request($id, $decoded['method'], $decoded['params']);
             } elseif (isset($decoded['result'])) {
